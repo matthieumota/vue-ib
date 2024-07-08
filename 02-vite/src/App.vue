3b77eba7bda2610dd72b045e3e86d73aaaf2ca63
Matthieu Mota <matthieumota@gmail.com>
2024-07-08 17:14:20 +0200

    Begin vite project and computed

---
+++ 02-vite/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>{{ title }}</h1>
+  {{ html }}
+  <span v-html="html"></span> <!-- Attention aux failles XSS -->
+
+  <!-- Le modifier permet de modifier les événements facilement -->
+  <input type="text" :value="title" @keyup.enter="title = $event.target.value">
+  <input type="text" v-model.lazy="title"> <!-- On change le titre au "change" -->
+  <button @['click']="title = 'Test'">Changer le titre</button>
+  <input type="text" v-model="title">
+
+  <input type="text" v-model="name">
+  <p>{{ name }}, Texte à l'envers: {{ reverse }}</p>
+
+  <ul>
+    <li v-for="letter in thirdLetters">
+      {{ letter }}
+    </li>
+  </ul>
+  <input type="checkbox" value="alban" v-model="letters"> Alban
+</template>
+
+<script setup>
+  import { computed, ref } from 'vue'
+
+  const title = ref('Mon application Vue JS')
+  const html = ref('<h2>Du HTML</h2>')
+
+  const name = ref('Fiorella');
+  const letters = ref(['baba', 'allo', 'dede', 'ello', 'charles']);
+
+  // Une computed est une valeur qui est calculée à partir d'une ref
+  const reverse = computed(() => {
+    console.log('call');
+    return name.value.split('').reverse().join('');
+  })
+
+  const sortedLetters = computed(() => {
+    // let array = ['a', 'b', 'c']
+    // let array2 = array // Ici, c'est le même tableau
+    // let array3 = [...array] // Ici, c'est une copie du tableau donc un 2ème tableau
+    return [...letters.value].sort((a, b) => {
+      console.log('Abc');
+      return a < b ? -1 : 1;
+    });
+  });
+
+  const thirdLetters = computed(() => {
+    return sortedLetters.value.slice(0, 3);
+  });
+</script>

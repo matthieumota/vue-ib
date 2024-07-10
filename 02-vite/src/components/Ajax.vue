ae8db2818a7fed7deb1114c034d865af9968f82c
Matthieu Mota <matthieumota@gmail.com>
2024-07-10 09:39:09 +0200

    Ajax

--- 02-vite/src/components/Ajax.vue
+++ 02-vite/src/components/Ajax.vue
@@ -4,20 +4,32 @@
 
   const users = ref([]);
   const loading = ref(false);
+  const hasError = ref(false);
+  const el = ref();
+  console.log(el.value); // Le h2 n'est pas présent
 
-  onMounted(() => {
+  onMounted(() => { // On attend que le composant soit "prêt" (le DOM est chargé)
+    console.log(el.value);
     loading.value = true;
+    hasError.value = false;
 
-    axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+    axios.get('https://jsonplaceholder.typicode.com/users').then(response => { // Quand tout va bien
       users.value = response.data;
+    }).catch(error => { // Quand il y a une erreur
+      hasError.value = true;
+    }).finally(() => { // Se lance dans tous les cas
       setTimeout(() => loading.value = false, 500);
     });
   })
 </script>
 
 <template>
-  <div v-if="loading" style="height: 200px; display: flex; align-items: center;">
+  <h2 ref="el">Ajax</h2>
+  <div v-if="loading" class="box">
     Chargement...
+  </div>
+  <div v-else-if="hasError" class="box">
+    Désolé, l'API n'est pas disponible...
   </div>
   <ul v-else>
     <li v-for="user in users" :key="user.id">
@@ -25,3 +37,11 @@
     </li>
   </ul>
 </template>
+
+<style scoped>
+  .box {
+    height: 200px;
+    display: flex;
+    align-items: center;
+  }
+</style>

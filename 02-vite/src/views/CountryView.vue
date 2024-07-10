e3624634d940b98b023c3dfee0743da18419bc31
Matthieu Mota <matthieumota@gmail.com>
2024-07-10 14:35:59 +0200

    Add search to countries

--- 02-vite/src/views/CountryView.vue
+++ 02-vite/src/views/CountryView.vue
@@ -3,24 +3,51 @@ import axios from 'axios';
 import { computed, onMounted, ref } from 'vue';
 
 const countries = ref([]);
+const language = ref('fra');
+const search = ref('');
 
 const filteredCountries = computed(() => {
+    // let objets = [{ name: 'Mota', firstname: 'Fiorella' }, { name: 'Mota', firstname: 'Marina' }];
+    // console.log(objets.map((element) => ({ ...element, fullname: element.firstname + ' ' + element.name })));
+
     return countries.value
-        .map((country) => ({ ...country, name: country.translations['fra'].common }))
+        .map((country) => ({ ...country, name: country.translations[language.value].common }))
         .sort((countryA, countryB) => countryA.name < countryB.name ? -1 : 1);
 });
 
-const languages = (country) => country.languages ? Object.values(country.languages): [];
-
 onMounted(() => {
+    fetchAllCountries();
+});
+
+const fetchAllCountries = () => {
     axios.get('https://restcountries.com/v3.1/all').then(response => {
         countries.value = response.data;
     });
-});
+}
+
+const fetchCountries = () => {
+    if (search.value.trim() === '') {
+        return fetchAllCountries();
+    }
+
+    axios.get(`https://restcountries.com/v3.1/name/${search.value}`).then(response => {
+        countries.value = response.data;
+    }).catch(() => fetchAllCountries());
+}
 </script>
 
 <template>
     <h1>Pays</h1>
+
+    <select v-model="language">
+        <option value="fra">Français</option>
+        <option value="ita">Italien</option>
+    </select>
+
+    <form @submit.prevent="fetchCountries">
+        <input type="text" v-model="search">
+        <button>Chercher</button>
+    </form>
 
     <ul>
         <li v-for="country in filteredCountries">
@@ -30,8 +57,8 @@ onMounted(() => {
                     {{ capital }}<template v-if="key < country.capital.length - 1">, </template>
                 </span>
             </p>
-            <p>Langues parlées: <span v-for="language, key in languages(country)">
-                {{ language }}<template v-if="key < languages(country).length - 1">, </template>
+            <p>Langues parlées: <span v-for="language, name, key in country.languages">
+                {{ language }}<template v-if="key < Object.keys(country.languages).length - 1">, </template>
             </span></p>
         </li>
     </ul>
